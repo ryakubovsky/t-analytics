ed987d6a4df06578d0025f1a784f12700b66d388
ryakubovsky <data.yakubovsky.rom@gmail.com>
2019-12-11 12:57:27 +0300

    YandexDirect OAuth commit #2

--- tproject/tapp/urls.py
+++ tproject/tapp/urls.py
@@ -4,10 +4,9 @@ from django.conf.urls import url, include
 from . import views
 
 
-app_name = 'social'
 urlpatterns = [
     # url(r"^$", views.index),
     url(r"^VKAuth/", views.VKAuth),
     url(r"^YandexDirect/", views.YandexDirectAuth),
-    url(r'', include('social_django.urls'))
+    url(r'', include('social_django.urls', namespace='social'))
 ]
